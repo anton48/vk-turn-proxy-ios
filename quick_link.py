7f8bd9f66b36305b605b1d629a42e16fe4c48817
vanya biceps <debil@mail.ru>
2026-05-11 14:50:33 +0300

    Backward compatibility

--- quick_link.py
+++ quick_link.py
@@ -92,7 +92,8 @@ def load_config(argv):
     if len(argv) > 1:
         path = argv[1]
         with open(path, "r", encoding="utf-8") as f:
-            return json.load(f)["settings"]
+            data = json.load(f)
+            return data.get("settings", data)
     return CONFIG
 
 
